0ebbeddbe21cbeeb40577e31d84f1f2b801b7a3d
martian17 <yakumodanshi@gmail.com>
2023-03-21 20:51:30 +0900

    Convert to typescript except for append_type() functions

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 //util functions. will refactor into util npm module in the future
 const isNode = typeof window === "undefined";
-const BufferFrom = isNode?(...args)=>{
-    return Buffer.from(...args);
-}:(...args)=>{
-    return new Uint8Array(...args);
-};
 
-const U8FromView = function(view){
+const U8FromView = function(view: ArrayBufferView): Uint8Array{
     if(view instanceof Uint8Array){
         return view;
     }
     return new Uint8Array(view.buffer,view.byteOffset,view.byteLength);
 };
 
-const allocUnsafe = isNode?function(n){
+const allocUnsafe: (n: number) => Uint8Array = isNode?function(n){
     return new Uint8Array(Buffer.allocUnsafe(n).buffer,0,n);
 }:function(n){
     return new Uint8Array(new ArrayBuffer(n));
@@ -23,9 +18,9 @@ const VERBOSE = false;
 
 
 export class BufferBuilder{
-    length = 0;
-    u8;
-    constructor(size=512,length=0){
+    length:number = 0;
+    u8: Uint8Array;
+    constructor(size: number = 512,length: number = 0){
         this.u8 = allocUnsafe(size);
         this.length = length;
         if(length !== 0){
@@ -44,24 +39,22 @@ export class BufferBuilder{
         u8n.set(this.u8);
         this.u8 = u8n;
     }
-    grow(n){
+    grow(n: number){
         this.length += n;
         this.alloc();
     }
-    growIfNoSpace(offset,insertLength){
+    growIfNoSpace(offset: number, insertLength: number){
         const newalloc = offset + insertLength - this.length;
         if(newalloc > 0){
             this.grow(newalloc);
         }
     }
-    // interface view
-    // {buffer,byteLength:int,byteOffser:int}
-    set_buffer(view,offset = 0){
+    set_buffer(view: ArrayBufferView, offset: number = 0){
         const u8 = U8FromView(view);
         this.growIfNoSpace(offset,u8.length);
         this.u8.set(u8,offset);
     }
-    set_BE16_buffer(view,offset = 0){
+    set_BE16_buffer(view: ArrayBufferView, offset: number = 0){
         const u8 = U8FromView(view);
         this.growIfNoSpace(offset,u8.length);
         for(let i = 0; i+1 < u8.length; i+=2){
@@ -69,7 +62,7 @@ export class BufferBuilder{
             this.u8[offset+i+1] = u8[i];
         }
     }
-    set_BE32_buffer(view,offset = 0){
+    set_BE32_buffer(view: ArrayBufferView, offset: number = 0){
         const u8 = U8FromView(view);
         this.growIfNoSpace(offset,u8.length);
         for(let i = 0; i+3 < u8.length; i+=4){
@@ -79,7 +72,7 @@ export class BufferBuilder{
             this.u8[offset+i+3] = u8[i];
         }
     }
-    set_BE64_buffer(view,offset = 0){
+    set_BE64_buffer(view: ArrayBufferView, offset: number = 0){
         const u8 = U8FromView(view);
         this.growIfNoSpace(offset,u8.length);
         for(let i = 0; i+7 < u8.length; i+=8){
@@ -94,39 +87,49 @@ export class BufferBuilder{
         }
     }
 
-    append_buffer(buff){
+    append_buffer(buff: ArrayBufferView){
         this.set_buffer(buff,this.length);
     }
-    append_BE16_buffer(buff){
+    append_BE16_buffer(buff: ArrayBufferView){
         this.set_BE16_buffer(buff,this.length);
     }
-    append_BE32_buffer(buff){
+    append_BE32_buffer(buff: ArrayBufferView){
         this.set_BE32_buffer(buff,this.length);
     }
-    append_BE64_buffer(buff){
+    append_BE64_buffer(buff: ArrayBufferView){
         this.set_BE64_buffer(buff,this.length);
     }
-    export(){
+    export_u8(): Uint8Array {
         if(VERBOSE)console.log("exported length:",this.length);
         if(VERBOSE)console.log("background buffer length:",this.u8.length);
-        return BufferFrom(this.u8.buffer,0,this.length);
+        return this.u8.subarray(0,this.length);
+    }
+    export_buffer(): Buffer {
+        return Buffer.from(this.u8,0,this.length);
+    }
+    export(): Uint8Array | Buffer {
+        if(isNode){
+            return Buffer.from(this.u8,0,this.length);
+        }else{
+            return this.u8.subarray(0,this.length);
+        }
     }
 };
 
 const BE_writers = [];
-BE_writers[2] = function(u81,u82,offset){
+BE_writers[2] = function(u81: Uint8Array, u82: Uint8Array, offset: number){
     u81[offset] = u82[1];
     u81[offset+1] = u82[0];
-}
+};
 
-BE_writers[4] = function(u81,u82,offset){
+BE_writers[4] = function(u81: Uint8Array, u82: Uint8Array, offset: number){
     u81[offset] = u82[3];
     u81[offset+1] = u82[2];
     u81[offset+2] = u82[1];
     u81[offset+3] = u82[0];
-}
+};
 
-BE_writers[8] = function(u81,u82,offset){
+BE_writers[8] = function(u81: Uint8Array, u82: Uint8Array, offset: number){
     u81[offset] = u82[7];
     u81[offset+1] = u82[6];
     u81[offset+2] = u82[5];
@@ -135,8 +138,7 @@ BE_writers[8] = function(u81,u82,offset){
     u81[offset+5] = u82[2];
     u81[offset+6] = u82[1];
     u81[offset+7] = u82[0];
-}
-
+};
 
 for(let [typename,typearr] of [
     ["U8" ,new Uint8Array(1)],
@@ -148,21 +150,21 @@ for(let [typename,typearr] of [
     ["I32",new Int32Array(1)],
     ["I64",new BigInt64Array(1)],
     ["F32",new Float32Array(1)],
-    ["F64",new Float64Array(1)]]){
+    ["F64",new Float64Array(1)]] as const){
     const typesize = typearr.buffer.byteLength;
     const u8 = new Uint8Array(typearr.buffer);
-    BufferBuilder.prototype["append_"+typename] = function(val){
+    BufferBuilder.prototype["append_"+typename] = function(val: number|bigint){
         typearr[0] = val;
         this.grow(typesize);
         this.u8.set(new Uint8Array(typearr.buffer),this.length-typesize);
     }
-    BufferBuilder.prototype["set_"+typename] = function(val,offset){
+    BufferBuilder.prototype["set_"+typename] = function(val: number|bigint, offset: number){
         typearr[0] = val;
         this.growIfNoSpace(offset,typesize);
         this.set_buffer(typearr,offset);
     }
     if(typesize === 1)continue;
-    BufferBuilder.prototype["set_"+typename+"_aligned"] = function(val,offset){
+    BufferBuilder.prototype["set_"+typename+"_aligned"] = function(val: number|bigint, offset: number){
         typearr[0] = val;
         offset *= typesize;
         this.growIfNoSpace(offset,typesize);
@@ -170,18 +172,18 @@ for(let [typename,typearr] of [
     }
 
     const BE_writer = BE_writers[typesize];
-    BufferBuilder.prototype["append_"+typename+"BE"] = function(val){
+    BufferBuilder.prototype["append_"+typename+"BE"] = function(val: number|bigint){
         const length0 = this.length;
         typearr[0] = val;
         this.grow(typesize);
         BE_writer(this.u8,u8,length0);
     }
-    BufferBuilder.prototype["set_"+typename+"BE"] = function(val,offset){
+    BufferBuilder.prototype["set_"+typename+"BE"] = function(val: number|bigint, offset: number){
         typearr[0] = val;
         this.growIfNoSpace(offset,typesize);
         BE_writer(this.u8,u8,offset);
     }
-    BufferBuilder.prototype["set_"+typename+"BE_aligned"] = function(val,offset){
+    BufferBuilder.prototype["set_"+typename+"BE_aligned"] = function(val: number|bigint, offset: number){
         typearr[0] = val;
         offset *= typesize;
         this.growIfNoSpace(offset,typesize);
